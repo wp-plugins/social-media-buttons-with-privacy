afae17a402eb1b64a59781126b677ba60fa8ac33
tas2580 <tas2580@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-24 17:53:16 +0000

    Display  the buttons only on a single page
    git-svn-id: https://plugins.svn.wordpress.org/social-media-buttons-with-privacy/trunk@1074941 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Social Media with privacy
 Plugin URI: https://tas2580.net/downloads/download-1.html
 Description: Adds Social Media Bottons to each post
-Version: 1.1.1
+Version: 1.1.2
 Author: tas2580
 Author URI: https://tas2580.net
 License: GPLv2
@@ -11,32 +11,44 @@ License: GPLv2
 
 
 /*
-*
 * Output backend
-*
 */
-
 load_plugin_textdomain('tas2580', false, 'private_socialmedia/i18n');
-
 add_action('admin_menu', 'tas2580_smb_add_admin_menu');
 add_action('admin_init', 'tas2580_smb_settings_init');
 
-
+/**
+ * Add menu item to the admin pannel
+ */
 function tas2580_smb_add_admin_menu()
 { 
 	add_options_page(__('Social Media Buttons', 'tas2580'), __('Social Media Buttons', 'tas2580'), 'manage_options', 'social_media_buttons', 'tas2580_smb_options_page');
 }
 
-
+/**
+ * Generate the page
+ */
 function tas2580_smb_settings_init()
 { 
 	register_setting('pluginPage', 'tas2580_smb_settings');
 	add_settings_section('tas2580_smb_settings', __('Settings', 'tas2580'), 'tas2580_smb_settings_section_callback', 'pluginPage');
 	add_settings_field('tas2580_smb_platforms', __('Choose platforms', 'tas2580'), 'tas2580_smb_platforms_render', 'pluginPage', 'tas2580_smb_settings');
 	add_settings_field('tas2580_smb_cachetime', __('Cachetime', 'tas2580'), 'tas2580_smb_cachetime_render', 'pluginPage', 'tas2580_smb_settings');
-
 }
 
+function tas2580_smb_options_page()
+{
+	echo'<form action="options.php" method="post"><h2>' . __('Social Media Buttons' ,'tas2580') . '</h2>';
+	settings_fields('pluginPage');
+	do_settings_sections('pluginPage');
+	submit_button();
+	echo '</form>';
+}
+
+
+/**
+ * The Cache setting
+ */
 function tas2580_smb_cachetime_render() 
 {
 	$options = get_option('tas2580_smb_settings');
@@ -47,9 +59,17 @@ function tas2580_smb_cachetime_render()
 		<option value="60" <?php selected($options['cachemultiplicator'], 60); ?>><?php _e('Minutes', 'tas2580'); ?></option>
 		<option value="3600" <?php selected($options['cachemultiplicator'], 3600); ?>><?php _e('Hours', 'tas2580'); ?></option>
 	</select>
-<?php
+	<?php
 }
 
+function tas2580_smb_settings_section_callback()
+{ 
+	echo __('Select the platforms you want to use and set the cache time.', 'tas2580');
+}
+
+/**
+ * Select platforms setting
+ */
 function tas2580_smb_platforms_render()
 { 
 	$options = get_option('tas2580_smb_settings');
@@ -71,66 +91,66 @@ function tas2580_smb_platforms_render()
 }
 
 
-function tas2580_smb_settings_section_callback()
-{ 
-	echo __('Select the platforms you want to use and set the cache time.', 'tas2580');
-}
 
 
 
-function tas2580_smb_options_page()
-{
-	echo'<form action="options.php" method="post"><h2>' . __('Social Media Buttons' ,'tas2580') . '</h2>';
-	settings_fields('pluginPage');
-	do_settings_sections('pluginPage');
-	submit_button();
-	echo '</form>';
-}
-
-/*
-*
+/**
 * Output frontend
-*
 */
 
 add_filter('the_content', 'tas2580_social_buttons');
 wp_enqueue_style('social_media', plugins_url('css/style.css', __FILE__), array(), 1.0, 'screen');
 
-// Output Buttons to content
+/**
+ * Add Buttons to every single page
+ * 
+ * @param	string	$content	The page content
+ */
 if(!function_exists('tas2580_social_buttons'))
 {
 	function tas2580_social_buttons($content)
 	{
-		$url = urlencode(get_permalink($post->ID));
-		$title = get_the_title($post->ID);
-		$shares = tas2580_get_share_count($url);
-		$options = get_option('tas2580_smb_settings');
+		if(is_single())
+		{
+			$url = urlencode(get_permalink($post->ID));
+			$title = get_the_title($post->ID);
+			$shares = tas2580_get_share_count($url);
+			$options = get_option('tas2580_smb_settings');
 
-		$buttons = '<strong>' . __('Social Media', 'tas2580') . '</strong><br /><script>function open_socialmedia(url){window.open(url,\'\',\'width=500, height=600\');return false;}</script>';
+			$buttons_title = '<strong>' . __('Social Media', 'tas2580') . '</strong><br /><script>function open_socialmedia(url){window.open(url,\'\',\'width=500, height=600\');return false;}</script>';
 
-		if(isset($options['facebook']) && ($options['facebook'] == 1))
-		{
-			$buttons .= '<a href="#" onclick="return open_socialmedia(\'https://www.facebook.com/sharer/sharer.php?u=' . $url . '\');" title="' . __('Share on Facebook', 'tas2580') . '" class="socialmedia1">&nbsp;</a><span class="social_count">' . (int) $shares['facebook'] . '</span>';
+			$buttons = '';
+			if(isset($options['facebook']) && ($options['facebook'] == 1))
+			{
+				$buttons .= '<a href="#" onclick="return open_socialmedia(\'https://www.facebook.com/sharer/sharer.php?u=' . $url . '\');" title="' . __('Share on Facebook', 'tas2580') . '" class="socialmedia1">&nbsp;</a><span class="social_count">' . (int) $shares['facebook'] . '</span>';
+			}
+			if(isset($options['twitter']) && ($options['twitter'] == 1))
+			{
+				$buttons .= '<a href="#" onclick="return open_socialmedia(\'https://twitter.com/intent/tweet?text=' . $title . '&amp;url=' . $url . '\');" title="' . __('Share on Twitter', 'tas2580') . '" class="socialmedia2">&nbsp;</a><span class="social_count">' . (int) $shares['twitter'] . '</span>';
+			}
+			if(isset($options['google']) && ($options['google'] == 1))
+			{
+				$buttons .= '<a href="#" onclick="return open_socialmedia(\'https://plus.google.com/share?url=' . $url . '&amp;title=' . $title . '\');" title="' . __('Share on Google+', 'tas2580') . '" class="socialmedia3">&nbsp;</a><span class="social_count">' . (int) $shares['google'] . '</span>';
+			}
+			if(isset($options['linkedin']) && ($options['linkedin'] == 1))
+			{
+				$buttons .= '<a href="#" onclick="return open_socialmedia(\'http://www.linkedin.com/shareArticle?mini=true&amp;url=' . $url . '&amp;title=' . $title . '\');" title="' . __('Share on Linkedin', 'tas2580') . '" class="socialmedia4">&nbsp;</a><span class="social_count">' . (int) $shares['linkedin'] . '</span>';
+			}
+			if(!empty($buttons))
+			{
+				$content = $content . $buttons_title . $buttons;
+			}
 		}
-		if(isset($options['twitter']) && ($options['twitter'] == 1))
-		{
-			$buttons .= '<a href="#" onclick="return open_socialmedia(\'https://twitter.com/intent/tweet?text=' . $title . '&amp;url=' . $url . '\');" title="' . __('Share on Twitter', 'tas2580') . '" class="socialmedia2">&nbsp;</a><span class="social_count">' . (int) $shares['twitter'] . '</span>';
-		}
-		if(isset($options['google']) && ($options['google'] == 1))
-		{
-			$buttons .= '<a href="#" onclick="return open_socialmedia(\'https://plus.google.com/share?url=' . $url . '&amp;title=' . $title . '\');" title="' . __('Share on Google+', 'tas2580') . '" class="socialmedia3">&nbsp;</a><span class="social_count">' . (int) $shares['google'] . '</span>';
-		}
-		if(isset($options['linkedin']) && ($options['linkedin'] == 1))
-		{
-			$buttons .= '<a href="#" onclick="return open_socialmedia(\'http://www.linkedin.com/shareArticle?mini=true&amp;url=' . $url . '&amp;title=' . $title . '\');" title="' . __('Share on Linkedin', 'tas2580') . '" class="socialmedia4">&nbsp;</a><span class="social_count">' . (int) $shares['linkedin'] . '</span>';
-		}
-
-		$content = $content . $buttons;
 		return $content;
 	}
 }
 
-// Count shares on social media platforms
+/**
+ * Count shares on social media platforms
+ * 
+ * @param	string	$url	URL to share
+ * @return	array	Number of the shares
+ */
 if(!function_exists('tas2580_get_share_count'))
 {
 	function tas2580_get_share_count($url)
--- readme.txt
+++ readme.txt
@@ -35,22 +35,27 @@ cache the requested data.
 1. Upload `private_socialmedia` to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Make sure that `/wp-content/plugins/private_socialmedia/cache/` is writeable
+4. Go to the settings page and select the social media platformt that you want to use
 
 == Changelog ==
 
+= 1.1.2 =
+Release Date: January 24, 2015
+* Bug Fix: Display buttons only on single pages
+
 = 1.1.1 =
-Release Date: December 10, 2011
+Release Date: December 10, 2014
 * Enhancement: Added german translation
 * Bug Fix: Send URLs through urlencode()
 
 = 1.1.0 =
-Release Date: December 9, 2011
+Release Date: December 9, 2014
 * Enhancement: Added admin module to configure used platforms and cache time
 
 = 1.0.1 =
-Release Date: December 7, 2011
+Release Date: December 7, 2014
 * Bug Fix: Updated readme.txt
 
 = 1.0.0 =
-Release Date: December 7, 2011
+Release Date: December 7, 2014
 * Initial release
